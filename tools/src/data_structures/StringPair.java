33e051985c3feb9d84a576a02109a5e6f8590c5a
Mikhail Andreev <mikha.andreev@gmail.com>
2014-08-29 18:49:42 -0400

    Added data_structures package which will contain basic generic data structures. Currently populated by StringPair.

---
+++ tools/src/data_structures/StringPair.java
@@ -0,0 +1,9 @@
+package data_structures;
+
+public class StringPair {
+	public String first, second;
+	public StringPair(String n_first, String n_second){
+		first = n_first;
+		second = n_second;
+	}
+}
